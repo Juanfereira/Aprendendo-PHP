f88f90b52c79d474a51177fb5b17a239e290e72a
Juanfereira <juandeoferreira@gmail.com>
2025-02-02 22:27:43 -0300

    first commit

---
+++ Exercicio 01/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste para PHP</title>
+</head>
+<body>
+
+ <h1>Teste para ver se esta funcionando</h1>
+ <?php  
+ $idade ="10";
+ $sexo = "Masculino";
+
+ print "Tenho $idade anos de idade, e meu sexo é $sexo";
+ 
+?>
+ <p></p>
+ <?php 
+
+ 
+ 
+ ?>
+ <p>Olá Mundo</p>
+ 
+    
+</body>
+</html>
+
---
+++ ex003/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h1> Testes primitivos em PHP</h1>
+
+    <?php
+    //0x= hexadecimal 
+    //ob= binario
+    //echo "O valor da variavel é $num";
+
+    //$num = 3e2;
+   // echo "O valor da variavel é $num";
+
+   //$nun = (int)"950";
+   //var_dump($nun);
+   $casado = true;
+    //var_dump($casado);
+    print "O valor da variavel é $casado";
+    
+    ?>
+
+
+
+    
+</body>
+</html>
